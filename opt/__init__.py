3686e9465242e4f924593665ee27ccff3c8f8eee
Lussati Santos <lussatisantos10@gmail.com>
2023-08-01 16:58:23 +0100

    new opt

--- opt/__init__.py
+++ opt/__init__.py
@@ -1,8 +1,9 @@
 def main():
     print('''
-1 - Multiplication
-2 - Division
-3 - Sum
-4 - Subtraction
-5 - End of program
+1 - Registrar uma compra
+2 - Ultima factura
+3 - Mudar de utilizador
+4 - Fecho do programa
 ''')
+          
+          
