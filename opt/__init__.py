f9f43d4c18429ce38fa96c6bcd69900c2828955a
Lussati Santos <lussatisantos10@gmail.com>
2023-08-01 16:52:51 +0100

    defs
    importacao das def usadas no projecto virtual-calculator

---
+++ opt/__init__.py
@@ -0,0 +1,8 @@
+def main():
+    print('''
+1 - Multiplication
+2 - Division
+3 - Sum
+4 - Subtraction
+5 - End of program
+''')
---
+++ style/__init__.py
@@ -0,0 +1,13 @@
+def estilo(msg):
+    tam = len(msg)
+    print('='*tam)
+    print(msg)
+    print('='*tam)
+
+def linhas(msg):
+    print('-'*20)
+    print(msg)
+    print('-'*20)
+
+def linha():
+    print('-'*20)
