c6e50474ae859150b99d344ac9507990ce63cc18
Lussati Santos <lussatisantos10@gmail.com>
2023-08-02 19:06:19 +0100

    usr, senha, code

--- usuario/__init__.py
+++ usuario/__init__.py
@@ -3,5 +3,16 @@ nomes = ['Lussati Santos',
          'Alexandre Sambaka',
          'Adelino Lussati'
          ]
-         
+
+senha = ['46258d',
+         '447658',
+         '973213',
+         '867687'
+         ]
+
+code = ['ls3222',
+        'mp2438',
+        'as2126',
+        'al7981'
+        ]
 print(nomes[0])
